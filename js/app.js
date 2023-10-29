df368eca76955cf1c959126a499ac9dbad848749
ARMANDO BUGARIN <armando.bugarin24@gmail.com>
2023-10-29 07:27:27 -0700

    add final score alert, edit question 6

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 `use strict`;
-
+let userScore = 0;
 let user = prompt("Hello! What is your name?");
 console.log('Hi', user);
 document.write(`Hello ${user}, welcome to the site!`)
@@ -12,45 +12,49 @@ else {
     alert('Welcome!');
 }
 
-
-let oneQuestion = prompt("Is Denver the capital of Colorado? Answer yes/no").toLowerCase();
+// question 1
+let oneQuestion = prompt("I was born in Denver. Is Denver the capital of Colorado? Answer yes/no").toLowerCase();
 console.log('You answered', oneQuestion);
 
 if (oneQuestion == 'yes') {
     alert('Correct, Denver is the capital of Colorado');
+    userScore++;
 }
 else {
     alert('Not correct');
 }
 
-
-let twoQuestion = prompt("Are you able to skateboard? Answer YES/NO").toUpperCase();
+// question 2
+let twoQuestion = prompt("Did I live in Las Vegas prior to moving to Kirkland? Answer YES/NO").toUpperCase();
 console.log('You answered', twoQuestion);
 
 if (twoQuestion == 'YES') {
-    alert('That is impressive you know how to skateboard');
+    alert('Correct! Viva Las Vegas');
+    userScore++;
 }
 else if (twoQuestion != 'yes') {
-    alert('skateboarding is hard!');
+    alert('incorrect!');
 }
 
-
+// question 3
 let threeQuestion = prompt("Was I a real estate agent before this? Answer yes/no").toLowerCase();
 console.log('You answered', threeQuestion ,);
 
 if (threeQuestion == 'yes') {
     alert('That is true');
+    userScore++;
 }
 else if (twoQuestion != 'yes') {
     alert('Incorrect');
 }
 
-
+// question 4
 let fourQuestion = prompt("I have a bachelor's degree? Answer YES/NO").toUpperCase();
 console.log('You answered', fourQuestion);
 
 if (fourQuestion == 'YES') {
     alert('Correct');
+    userScore++;
 }
 else if (fourQuestion == 'NO') {
     alert('Incorrect');
@@ -59,12 +63,13 @@ else {
     alert('Wrong input');
 }
 
-
+// question 5
 let fiveQuestion = prompt("The Denver Nuggets are the best team in the NBA? Answer yes/no").toLowerCase();
 console.log('The Denver Nuggets are the best team:', fiveQuestion);
 
 if (fiveQuestion == 'yes') {
     alert('Correct, they are the best')
+    userScore++;
 } 
 else if (fiveQuestion == 'no') {
     alert('You know they are the best, do not lie');
@@ -74,26 +79,29 @@ else {
 }
 
 
+// question 6
 for (let i = 0; i < 4; i++) {
     let sibling = prompt("How many siblings do I have?");
-    parseInt(sibling);
-    if (sibling == 2) {
+    guessSiblings = parseInt(sibling);
+    if (guessSiblings == 2) {
         alert('Correct');
+        userScore++;
         break;
     }
-    else if (sibling > 2) {
+    else if (guessSiblings > 2) {
         alert('Too High');
     }
-    else if (sibling < 2) {
+    else if (guessSiblings < 2) {
         alert('Too Low');
     }
-    else {
+    if (i === 3) {
         alert('Wrong, I have 2 siblings');
     }
     console.log('I have 2 siblings')
 }  
 
 
+// question 7
 let cities = ["dubai", "bruges", "amsterdam", "cologne", "cambridge", "london"]
 let guessAttempt = 0
 let correctGuess = false;
@@ -108,6 +116,7 @@ while(guessAttempt < 6) {
         if (citiesInput === cities[i]) {
             alert('You got it!');
             correctGuess = true;
+            userScore++;
             break;
 
         } 
@@ -118,3 +127,7 @@ while(guessAttempt < 6) {
         alert('No, keep trying');
     }
 }
+
+// final score out of 7 questions
+
+alert(`You got ${userScore} correct out of 7`);
